8014848e4fb5cf57f78418394909b06709dd1422
SurajKukkipady <56889466+SurajKukkipady@users.noreply.github.com>
2025-06-06 11:25:29 +0530

    Update main.py

--- Day48/main.py
+++ Day48/main.py
@@ -1,11 +1,17 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://www.google.com")
+driver.get("https://appbrewery.github.io/instant_pot/")
 
-#driver.close()  # This will close the current window, but the browser will remain open if detach is set to True
+price_dollar = driver.find_element(By.CLASS_NAME, value="a-price-whole")
+price_cents = driver.find_element(By.CLASS_NAME, value="a-price-fraction")
+print(f"{price_dollar.text}.{price_cents.text}")
+
+
+
+# driver.close()  # This will close the current window
 driver.quit()  # This will close the browser and end the session
-
