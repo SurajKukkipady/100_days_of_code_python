c1d264c641d57c29b06b86102948b1fc05c4c775
SurajKukkipady <56889466+SurajKukkipady@users.noreply.github.com>
2025-06-10 15:27:22 +0530

    Update hello.py

--- Day54/hello.py
+++ Day54/hello.py
@@ -2,7 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+print(__name__)
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/hi")
+def hi_world():
+    return "<p>Hi, World!</p>"
+
+if __name__ == "__main__":
+    app.run()
 
