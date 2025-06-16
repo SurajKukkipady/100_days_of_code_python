5f87f9386cac0d644f37e0ae714df9d68858cd3f
SurajKukkipady <56889466+SurajKukkipady@users.noreply.github.com>
2025-06-16 15:06:08 +0530

    Update hello.py

--- Day55/hello.py
+++ Day55/hello.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from functools import wraps
 
 app = Flask(__name__)
 
@@ -6,11 +7,21 @@ print(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return ('<h1 style="text-align: center">Hello, World!</h1>'
+            '<p>This is a paragraph.</p>'
+            '<img src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExN2xkZXhvYWY3MDgwdTlnNDFqdWRocWgzdGNnOW1yNDBpczd3dmc4ZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/26DOJwj9M4npUa54A/giphy.gif" width=200>')
+
+def underline(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        result = f(*args, **kwargs)
+        return f"<u>{result}</u>"
+    return decorated_function
 
 @app.route("/bye")
+@underline
 def bye():
-    return "<p>Bye, World!</p>"
+    return "<b>Bye, World!</b>"
 
 @app.route("/<name>")
 def greet(name):
