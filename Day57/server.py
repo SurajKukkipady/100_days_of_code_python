b13e320f3205de91a60463c7223457d6589605a7
SurajKukkipady <56889466+SurajKukkipady@users.noreply.github.com>
2025-06-17 17:47:59 +0530

    task

--- Day57/server.py
+++ Day57/server.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 import random
 import datetime
+import requests
 
 
 app = Flask(__name__)
@@ -11,6 +12,21 @@ def hello_world():
     random_number = random.randint(1, 10)
     timestamp = datetime.datetime.now().strftime("%Y")
     return render_template("index.html", number=random_number, timestamp=timestamp)
+
+
+@app.route("/guess/<name>")
+def guess(name):
+    url_age = "https://api.agify.io"
+    url_gender = "https://api.genderize.io"
+    params = {"name": name}
+
+    response = requests.get(url_age, params=params)
+    age = response.json()['age']
+
+    response = requests.get(url_gender, params=params)
+    gender = response.json()['gender']
+
+    return render_template("guess.html", name=name, age=age, gender=gender)
 
 
 if __name__ == "__main__":
